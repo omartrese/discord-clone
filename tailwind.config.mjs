57ba26bb2eb047c3e70fc5a7770e2d5fdcd38c46
omartrese <omarlayachi13@gmail.com>
2024-09-29 20:46:13 +0200

    footer

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,10 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			'discordTitle': ['discordFont', 'sans-serif']
+			'discordTitle': ['discordFont', 'sans-serif'],
+			'ggSansNormal': ['ggSansNormal', 'sans-serif'],
+			'ggSansSemiBold': ['ggSansSemiBold', 'sans-serif'],
+			'ggSansExtraBold': ['ggSansExtraBold', 'sans-serif'],
 		},
 	},
 	plugins: [],
